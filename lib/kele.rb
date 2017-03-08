6d6c62d6a2fc22fcec3c6bf4e8778fca7e1404d8
thomaslawton91 <thomaslawton91@gmail.com>
2017-03-08 11:47:55 -0500

    Add mentor availability method. Ask mentor why recieving 'Resource not found'

--- lib/kele.rb
+++ lib/kele.rb
@@ -17,6 +17,12 @@ class Kele
     JSON.parse(response.body)
   end
 
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(bloc_api("#{mentor_id}/student_availability"), headers: {"authorization" => @auth_token })
+
+    JSON.parse(response.body)
+  end
+
   def bloc_api(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
   end
